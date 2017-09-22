b6a297238b1b91824171107119259ff19a17a12d
davet <dave.thomas@idsdigital.com>
2017-09-22 15:24:13 +0100

    added dave's class

---
+++ TestSol/TestSol/DavesClass.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSol
+{
+    public class DavesClass
+    {
+        private string s = "Dave is the Greatest";
+    }
+}
